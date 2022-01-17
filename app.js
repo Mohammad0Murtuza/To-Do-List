6818272d033dab6bb25b9fef530e7427bc56b4d7
Captain_Murtuza <69781075+Mohammad0Murtuza@users.noreply.github.com>
2022-01-17 18:31:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const addForm = document.getElementById("addForm");
+const itemList = document.getElementById("items");
+const del = document.querySelector(".delete");
+const filter = document.getElementById("filter");
+
+addForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let item = document.getElementById("item").value;
+
+    const li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(item));
+
+    const btn = document.createElement('button');
+    btn.className = 'delete btn btn-danger btn-sm float-right';
+    btn.appendChild(document.createTextNode("X"));
+    li.appendChild(btn);
+    itemList.appendChild(li);
+
+})
+
+itemList.addEventListener('click', function(e) {
+    if (e.target.classList.contains('delete')) {
+        const li = e.target.parentElement;
+        itemList.removeChild(li);
+    }
+})
+
+filter.addEventListener('keyup', function(e) {
+    const text = e.target.value.toLowerCase();
+
+    const items = itemList.getElementsByTagName('li');
+
+    Array.from(items).forEach(function(item){
+   		const itemName = item.firstChild.textContent;
+
+   		if(itemName.toLowerCase().indexOf(text) != -1){
+   			item.style.display = 'block';
+   		} else{
+   			item.style.display = 'none';
+   		}
+    })
+})
