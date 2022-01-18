96bd8252ea238ec03a565ce25fa319ebbada4ec5
Captain_Murtuza <69781075+Mohammad0Murtuza@users.noreply.github.com>
2022-01-18 15:12:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,22 +2,29 @@ const addForm = document.getElementById("addForm");
 const itemList = document.getElementById("items");
 const del = document.querySelector(".delete");
 const filter = document.getElementById("filter");
+const msg = document.querySelector(".msg");
 
 addForm.addEventListener('submit', function(e) {
     e.preventDefault();
 
     let item = document.getElementById("item").value;
 
-    const li = document.createElement('li');
-    li.className = 'list-group-item';
-    li.appendChild(document.createTextNode(item));
+    if (item.length>0) {
+        const li = document.createElement('li');
+        li.className = 'list-group-item';
+        li.appendChild(document.createTextNode(item));
 
-    const btn = document.createElement('button');
-    btn.className = 'delete btn btn-danger btn-sm float-right';
-    btn.appendChild(document.createTextNode("X"));
-    li.appendChild(btn);
-    itemList.appendChild(li);
+        const btn = document.createElement('button');
+        btn.className = 'delete btn btn-danger btn-sm float-right';
+        btn.appendChild(document.createTextNode("X"));
+        li.appendChild(btn);
+        itemList.appendChild(li);
 
+        document.getElementById("item").value=" ";
+    } else {
+        msg.textContent="Atleast Enter One Character";
+        setTimeout(() => {msg.textContent=" "}, 1000);
+    }
 })
 
 itemList.addEventListener('click', function(e) {
